686deec8175af9ec9ca000f4504387022ff282b9
ekgns33 <ekgns00421@gmail.com>
2025-02-08 23:25:07 +0900

    Initial commit

---
+++ src/main/kotlin/org/ekgns33/springmarket/SpringMarketApplication.kt
@@ -0,0 +1,11 @@
+package org.ekgns33.springmarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMarketApplication>(*args)
+}
---
+++ src/test/kotlin/org/ekgns33/springmarket/SpringMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ekgns33.springmarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMarketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
