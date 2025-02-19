3559bb63e4f68edc6332aba481b82d34544c9090
ekgns33 <ekgns00421@gmail.com>
2025-02-19 23:17:27 +0900

    :sparkles: feat : 컨트롤러 코드작성

---
+++ src/main/kotlin/org/ekgns33/springmarket/user/adapter/in/model/SignUpResponse.kt
@@ -0,0 +1,6 @@
+package org.ekgns33.springmarket.user.adapter.`in`.model
+
+import org.ekgns33.springmarket.common.BaseResponse
+
+data class SignUpResponse (val name:String, val password:String) {
+}
