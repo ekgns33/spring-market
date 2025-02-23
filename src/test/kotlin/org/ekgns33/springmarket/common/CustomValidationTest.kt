d41d0303b9af1317fca9dbdc480b2c22497228da
ekgns33 <ekgns00421@gmail.com>
2025-02-24 00:58:17 +0900

    :white_check_mark: test : 테스트 코드 작성

---
+++ src/test/kotlin/org/ekgns33/springmarket/common/CustomValidationTest.kt
@@ -0,0 +1,24 @@
+package org.ekgns33.springmarket.common
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+@DisplayName("Validation 함수 테스트")
+class CustomValidationTest {
+
+    @DisplayName("이메일 정규표현식 검증 테스트 - 이메일형식이 아니라면 false 반환")
+    @ParameterizedTest
+    @ValueSource(strings = ["example@gmailcom", "@gmail.com", "examplegmailcom", "emailemail.com"])
+    fun 이메일_검증_실패(email: String) {
+        assertFalse(CustomValidation.isEmail(email))
+    }
+
+    @DisplayName("이메일 정규표현식 검증 테스트 - 이메일 형식이면 true 반환")
+    @ParameterizedTest
+    @ValueSource(strings = ["example@gmail.com", "test@naver.com"])
+    fun 이메일_검증_성공(email: String) {
+        assertTrue(CustomValidation.isEmail(email))
+    }
+}
--- src/test/kotlin/org/ekgns33/springmarket/user/api/AuthApiTest.kt
+++ src/test/kotlin/org/ekgns33/springmarket/user/api/AuthApiTest.kt
@@ -1,13 +1,13 @@
 package org.ekgns33.springmarket.user.api
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.ekgns33.springmarket.user.adapter.`in`.model.SignUpResponse
-import org.ekgns33.springmarket.user.adapter.`in`.model.SignupRequest
-import org.ekgns33.springmarket.user.service.port.`in`.UserAuthUsecase
+import org.ekgns33.springmarket.auth.adapter.`in`.model.SignInRequest
+import org.ekgns33.springmarket.auth.adapter.`in`.model.SigninResponse
+import org.ekgns33.springmarket.auth.service.port.`in`.SigninUsecase
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.given
 import org.mockito.kotlin.any
+import org.mockito.kotlin.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -15,40 +15,43 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DisplayName("회원 API 테스트")
-class UserApiTest {
+@DisplayName("인증 API 테스트")
+class AuthApiTest {
+
     @MockitoBean
-    lateinit var userAuthUsecase: UserAuthUsecase
+    lateinit var signinUsecase: SigninUsecase
     @Autowired
     lateinit var mockMvc: MockMvc
     @Autowired
     lateinit var objectMapper: ObjectMapper
 
     @Test
-    @DisplayName("회원 가입 요청 성공")
-    fun 회원가입요청_성공() {
-        val signUpRequest = SignupRequest(
-            email = "test@example.com",
-            name = "사용자1",
-            password = "비밀번호1"
+    @DisplayName("로그인 요청 성공")
+    fun 로그인_요청_성공() {
+        val signinRequet = SignInRequest(
+            email = "example@gmail.com",
+            password = "test"
         )
-        given(userAuthUsecase.signup(any()))
-            .willReturn(SignUpResponse("test@example.com", "사용자1", "비밀번호1"))
+        given(signinUsecase.signin(any(), any()))
+            .willReturn(SigninResponse("token"))
 
         val result = mockMvc.perform(
-            MockMvcRequestBuilders.post("/api/v1/users")
+            MockMvcRequestBuilders.post("/api/v1/auth/signin")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(signUpRequest))
+                .content(objectMapper.writeValueAsString(signinRequet))
         )
         result
-            .andExpect(status().isCreated)
+            .andExpect(status().isOk)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andDo(print())
+            .andDo(MockMvcResultHandlers.print())
     }
+
+
+
 }
