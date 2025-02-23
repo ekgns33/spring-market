6bf9dbfb9af13b6fc7ded111e6cbd0966347526e
ekgns33 <ekgns00421@gmail.com>
2025-02-24 00:58:44 +0900

    :recycle: refactor : 코드 스타일 적용

--- src/main/kotlin/org/ekgns33/springmarket/common/BaseResponse.kt
+++ src/main/kotlin/org/ekgns33/springmarket/common/BaseResponse.kt
@@ -1,9 +1,9 @@
 package org.ekgns33.springmarket.common
 
-sealed class BaseResponse (
+sealed class BaseResponse(
     open val status: Boolean,
     open val message: String,
-){
+) {
 
     companion object {
         fun <T> success(data: T, message: String): SuccessResponse<T> =
@@ -13,13 +13,13 @@ sealed class BaseResponse (
             ErrorResponse(errorCode = errorCode, status = false, message = message)
     }
 
-    data class SuccessResponse<T> (
+    data class SuccessResponse<T>(
         val data: T?,
         override val status: Boolean,
         override val message: String = "success"
     ) : BaseResponse(status, message)
 
-    data class ErrorResponse (
+    data class ErrorResponse(
         val errorCode: ErrorCode,
         override val status: Boolean = false,
         override val message: String
--- src/main/kotlin/org/ekgns33/springmarket/common/LoggerFactory.kt
+++ src/main/kotlin/org/ekgns33/springmarket/common/LoggerFactory.kt
@@ -3,4 +3,4 @@ package org.ekgns33.springmarket.common
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-inline fun<reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)!!
+inline fun <reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)!!
