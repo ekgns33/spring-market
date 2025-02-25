07d109fdf8126ea772cfba26f503e58a1587c4ba
ekgns33 <ekgns00421@gmail.com>
2025-02-25 16:37:43 +0900

    :recycle: refactor : 코드 import, style 수정

--- src/main/kotlin/org/ekgns33/springmarket/product/service/port/in/ProductRegisterCommand.kt
+++ src/main/kotlin/org/ekgns33/springmarket/product/service/port/in/ProductRegisterCommand.kt
@@ -1,8 +1,8 @@
 package org.ekgns33.springmarket.product.service.port.`in`
 
-import org.ekgns33.springmarket.product.domain.ProductStatus
 import org.ekgns33.springmarket.common.Money
 import org.ekgns33.springmarket.product.domain.Product
+import org.ekgns33.springmarket.product.domain.ProductStatus
 
 data class ProductRegisterCommand(
     val name: String,
