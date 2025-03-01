44844a54dfa56df40f1a9697a74ec7de9919d346
ekgns33 <ekgns00421@gmail.com>
2025-03-01 13:09:42 +0900

    :sparkles: feat : 금액 곱하기 함수 추가

--- src/main/kotlin/org/ekgns33/springmarket/common/Money.kt
+++ src/main/kotlin/org/ekgns33/springmarket/common/Money.kt
@@ -3,4 +3,8 @@ package org.ekgns33.springmarket.common
 data class Money(
     var value: Int = 0,
 ) {
+
+    fun multiply(multiplier: Int): Money {
+        return Money(value * multiplier)
+    }
 }
